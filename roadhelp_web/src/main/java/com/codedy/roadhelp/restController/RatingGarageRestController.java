186bc01851eb597c55f5a748d9c3b8e062be14d6
Vũ Quang Huy <vuquanghuyxl1234@gmail.com>
2021-09-30 10:32:21 +0700

    UPDATE: sửa request mapping trong restcontroller

--- roadhelp_web/src/main/java/com/codedy/roadhelp/restController/RatingGarageRestController.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/restController/RatingGarageRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/garages")
+@RequestMapping(path = "/api/v1/ratinggarages")
 public class RatingGarageRestController {
 
     @Autowired
