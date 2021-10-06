a08acbf37b92d934fbe44d8a479dad486a57828c
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-06 17:20:28 +0700

    NEW: Cấu hình routes & Thêm tất cả màn hình còn thiếu (màn hình trống)

---
+++ roadhelp_app/lib/screens/emergency_rescue/send/send_rescue/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text("Make me go"),
+      ),
+    );
+  }
+}
---
+++ roadhelp_app/lib/screens/my_account/my_account/my_account_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class MyAccountScreen extends StatelessWidget {
+  static String routeName = "/route_name";
+
+  const MyAccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title - Change me now"),
+      ),
+      body: Body(),
+    );
+  }
+}
