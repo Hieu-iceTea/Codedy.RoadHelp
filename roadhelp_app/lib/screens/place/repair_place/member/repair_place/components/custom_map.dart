286e4be93080b41a8b07b2fa3a95bc108e40a4f0
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-08 01:32:49 +0700

    NEW: RepairPlaceScreen - UI

---
+++ roadhelp_app/lib/screens/place/repair_place/member/repair_place/components/custom_map.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:roadhelp/helper/location_helper.dart';
+
+class CustomMap extends StatefulWidget {
+  LatLng? initialCameraPosition;
+  ArgumentCallback<LatLng>? onTap;
+  Set<Marker> markers;
+
+  CustomMap({
+    this.initialCameraPosition,
+    this.onTap,
+    this.markers = const {},
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomMap> createState() => _CustomMapState();
+}
+
+class _CustomMapState extends State<CustomMap> {
+  LatLng? _pickedLocation;
+  late Set<Marker> _markers;
+
+  @override
+  void initState() {
+    _markers = widget.markers;
+    if (widget.initialCameraPosition == null) {
+      _getInitialCameraPosition();
+    }
+    super.initState();
+  }
+
+  Future<void> _getInitialCameraPosition() async {
+    var currentLocation = await LocationHelper.getCurrentLocation();
+    setState(() {
+      if (currentLocation == null) {
+        widget.initialCameraPosition =
+            const LatLng(21.0291518, 105.8523056); //Hồ Gươm, Hà Nội
+        return;
+      }
+      widget.initialCameraPosition =
+          LatLng(currentLocation.latitude!, currentLocation.longitude!);
+    });
+  }
+
+  ArgumentCallback<LatLng>? _onTap(LatLng position) {
+    if (widget.onTap == null) {
+      return null;
+    }
+
+    setState(() {
+      _pickedLocation = position;
+    });
+
+    _markers = {
+      ...widget.markers,
+      Marker(
+          markerId: const MarkerId('marker_picked_1'),
+          position: _pickedLocation!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueOrange)),
+    };
+
+    widget.onTap!(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.initialCameraPosition == null
+        ? const Center(child: CircularProgressIndicator())
+        : GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: widget.initialCameraPosition!,
+              zoom: 16,
+            ),
+            onTap: _onTap,
+            markers: _markers,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            padding: EdgeInsets.only(
+              top: 60,
+              bottom: 100,
+            ),
+          );
+  }
+}
--- roadhelp_app/lib/screens/place/repair_place/member/repair_place/repair_place_screen.dart
+++ roadhelp_app/lib/screens/place/repair_place/member/repair_place/repair_place_screen.dart
@@ -1,19 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:roadhelp/components/rounded_icon_btn.dart';
+import 'package:roadhelp/config/size_config.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import 'components/body.dart';
 
 class RepairPlaceScreen extends StatelessWidget {
-  static String routeName = "/route_name";
+  static String routeName = "/repair_place";
 
   const RepairPlaceScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // You have to call it on your starting screen:
+    SizeConfig().init(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Title - Change me now"),
+      body: SlidingUpPanel(
+        //backdropEnabled: true,
+        collapsed: _panelHeader(opacity: 1),
+        maxHeight: SizeConfig.screenHeight * .75,
+        minHeight: 95.0,
+        parallaxEnabled: true,
+        parallaxOffset: .5,
+        header: _panelHeader(opacity: 0.9),
+        panel: SingleChildScrollView(
+          child: Column(
+            children: [_panelBody()],
+          ),
+        ),
+        //panelBuilder: (scrollController) => _panel(scrollController),
+        body: Body(),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18.0),
+          topRight: Radius.circular(18.0),
+        ),
       ),
-      body: Body(),
+    );
+  }
+
+  Widget _panelHeader({required double opacity}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(opacity),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.15),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      width: SizeConfig.screenWidth,
+      child: Column(
+        children: [
+          SizedBox(height: 12.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 30,
+                height: 5,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              ),
+            ],
+          ),
+          SizedBox(height: 18.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "List Repair Place",
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 24.0,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 18.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _panelBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white60.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.15),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            padding: EdgeInsets.only(top: 55),
+            child: ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (ctx, index) => Card(
+                child: ListTile(
+                  onTap: () {},
+                  leading: FlutterLogo(size: 72.0),
+                  title: Text('Garage Name ($index)'),
+                  subtitle: Text(
+                      'Số điện thoại\nĐịa chỉ | Khoảng cách'),
+                  trailing: RoundedIconBtn(
+                    icon: Icons.directions,
+                    showShadow: true,
+                    press: () {},
+                  ),
+                  isThreeLine: true,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
