409b52e994ff095b3a2faa69344b8b871c1348af
Vũ Quang Huy <vuquanghuyxl1234@gmail.com>
2021-10-19 14:06:15 +0700

    NEW: đăng nhập và phân quyền

--- roadhelp_web/src/main/java/com/codedy/roadhelp/repository/UserRepository.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.codedy.roadhelp.repository;
 import com.codedy.roadhelp.model.User;
 
 public interface UserRepository extends BaseRepository<User, Integer>{
-
+    User findByUsername(String username);
 }
--- roadhelp_web/src/main/java/com/codedy/roadhelp/service/user/UserServiceImplement.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/service/user/UserServiceImplement.java
@@ -2,14 +2,23 @@ package com.codedy.roadhelp.service.user;
 
 
 import com.codedy.roadhelp.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import com.codedy.roadhelp.repository.UserRepository;
 import com.codedy.roadhelp.service.base.BaseServiceImplement;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 @Service
-public class UserServiceImplement extends BaseServiceImplement<User, Integer> implements UserService {
+@Slf4j
+public class UserServiceImplement extends BaseServiceImplement<User, Integer> implements UserService, UserDetailsService {
 
     //region Initialization - Autowired
     @Autowired
@@ -18,6 +27,23 @@ public class UserServiceImplement extends BaseServiceImplement<User, Integer> im
     public UserServiceImplement(UserRepository repository) {
         super(repository);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getAuthorities().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
     //endregion
+
 
 }
