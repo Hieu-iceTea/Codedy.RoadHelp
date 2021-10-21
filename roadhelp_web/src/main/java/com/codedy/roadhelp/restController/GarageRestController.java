fae51a806348512c5f4f6af66039093c7d7e4b26
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-10-21 22:43:51 +0700

    Update Api garage

--- roadhelp_web/src/main/java/com/codedy/roadhelp/restController/GarageRestController.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/restController/GarageRestController.java
@@ -6,6 +6,7 @@ import com.codedy.roadhelp.service.garage.GarageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -60,5 +61,70 @@ public class GarageRestController {
 
         garageService.deleteById(id);
         return "Deleted garage id - " + id;
+    }
+
+
+    // Chi tiết tiệm sửa xe
+    @GetMapping(path = {"/repair-place-manage/{id}", "/repair-place-manage/{id}/"})
+    public Garage garageDetails(@PathVariable int id) {
+        Garage garage = garageService.findById(id);
+        if (garage == null) {
+            throw new RestNotFoundException("Garage id not found - " + id);
+        }
+        return garage;
+    }
+    // Thêm tiệm sửa xe
+    @PostMapping(path = {"/repair-place-manage", "/repair-place-manage/"})
+    public Garage storeGarage(@RequestBody Garage garage) {
+        garage.setId(0);
+        Garage newGarage = garageService.save(garage);
+        return garageService.findById(newGarage.getId());
+    }
+
+    // Danh sách tiệm sửa xe đang quản lý
+    @GetMapping(path = {"/repair-place-manage", "/repair-place-manage/"})
+    public List<Garage> listGarage(@RequestParam(defaultValue = "0") int PartnerId) {
+        List<Garage> garages = garageService.findAll();
+        List<Garage> garagesByProvinceId = new ArrayList<>();
+        for (Garage d : garages
+        ) {
+            if (d.getProvince().getId() == PartnerId) {
+                garagesByProvinceId.add(d);
+            }
+
+        }
+
+        if (PartnerId > 0) {
+            return garagesByProvinceId;
+        } else {
+            return garageService.findAll();
+        }
+    }
+
+
+    // Chi tiết tiệm sửa xe - Chỉnh sửa
+    @PutMapping(path = {"/repair-place-manage/{id}", "/repair-place-manage/{id}/"})
+    public Garage repair_update(@RequestBody Garage garage, @PathVariable int id) {
+
+        if (garageService.findById(id) == null) {
+            throw new RestNotFoundException("Garage id not found - " + id);
+        }
+        garage.setId(id);
+        garageService.save(garage);
+
+        return garageService.findById(garage.getId());
+    }
+
+    // Chi tiết tiệm sửa xe - Xóa / Tạm dừng hoạt động
+    @PutMapping(path = {"/repair-place-manage/{Id}/setActive", "/repair-place-manage/{Id}/setActive/"})
+    public String repair_delete(@PathVariable int Id,@RequestParam(value="setActive", required = false,
+            defaultValue = "true") boolean setActive) {
+
+        Garage garage = garageService.findById(Id);
+        garage.setActive(setActive);
+        garageService.save(garage);
+
+        return   " garage id - " + garage.getId() + " Set active - " + setActive;
+
     }
 }
