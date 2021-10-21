66a7d68d44651964f9800d4998ce2d301a20d1bd
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-10-22 00:40:01 +0700

    Update Api garage, Issue, ratting

--- roadhelp_web/src/main/java/com/codedy/roadhelp/restController/IssueRestController.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/restController/IssueRestController.java
@@ -1,11 +1,14 @@
 package com.codedy.roadhelp.restController;
 
 import com.codedy.roadhelp.model.Issue;
+import com.codedy.roadhelp.model.User;
+import com.codedy.roadhelp.model.enums.IssueStatus;
 import com.codedy.roadhelp.restController.exception.RestNotFoundException;
 import com.codedy.roadhelp.service.issues.IssuesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -30,7 +33,39 @@ public class IssueRestController {
         }
         return issue;
     }
+    // tạo thông báo
+    @PostMapping(path = {"/rescue/send", "/rescue/send/"})
+    public Issue createRescue(@RequestBody Issue issue) {
+        issue.setId(0);
+        Issue newIssue = issuesService.save(issue);
+        return issuesService.findById(newIssue.getId());
+    }
+    // Xem thông tin người giúp đỡ mình
+    @GetMapping(path = {"/rescue/send/confirmation", "/rescue/send/confirmation/"})
+    public User showComfirmation(@RequestParam(defaultValue = "0") int id) {
+        Issue issue = issuesService.findById(id);
+        if (issue == null) {
+            throw new RestNotFoundException("Issues id not found - " + id);
+        }
+        return issue.getUserPartners();
+    }
+    // Xác nhận thông tin người giúp đỡ mình
+    @PutMapping(path = {"/rescue/send/confirmation", "/rescue/send/confirmation/"})
+    public String acceptComfirmation(@RequestBody Issue issue , @RequestParam(defaultValue = "0") int id) {
 
+       // String statusRequest = requestBody.get("status");
+        if(issue.getStatus().toString() == IssueStatus.memberConfirmPartner.toString() ){
+             issue = issuesService.findById(id);
+            if (issuesService.findById(id) == null) {
+                throw new RestNotFoundException("Issuues id not found - " + id);
+            }
+            issue.setId(id);
+            issue.setStatus(IssueStatus.succeeded);
+            issuesService.save(issue);
+            return "xác nhận " + issue.getUserPartners().getLastName() + " " + issue.getUserPartners().getFirstName() + " là người hỗ trợ!";
+        }else return "sai status xác nhận! " +IssueStatus.memberConfirmPartner.toString();
+
+    }
     // Create Issues
     @PostMapping(path = {"", "/"})
     public Issue store(@RequestBody Issue issue) {
@@ -46,7 +81,6 @@ public class IssueRestController {
         if (issuesService.findById(id) == null) {
             throw new RestNotFoundException("Issuues id not found - " + id);
         }
-
         issue.setId(id);
         issuesService.save(issue);
         return issuesService.findById(issue.getId());
