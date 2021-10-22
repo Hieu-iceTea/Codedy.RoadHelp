21934204afd852f5233bd43c740f9521cf668acd
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-22 10:35:46 +0700

    FIX: Thêm Consumer() Provider vào chi tiết garage

--- roadhelp_app/lib/screens/place/repair_place/repair_place_details/components/body.dart
+++ roadhelp_app/lib/screens/place/repair_place/repair_place_details/components/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:roadhelp/components/rounded_icon_btn.dart';
 import 'package:roadhelp/config/size_config.dart';
 import 'package:roadhelp/models/garage.dart';
+import 'package:roadhelp/providers/garage_provider.dart';
 
 import '/components/default_button.dart';
 import 'garage_description.dart';
@@ -15,92 +17,97 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        SliderImages(garage: garage),
-        TopRoundedContainer(
-          color: Colors.white,
-          child: Column(
-            children: [
-              GarageDescription(
-                garage: garage,
-                pressOnSeeMore: () {},
-              ),
-              TopRoundedContainer(
-                color: Color(0xFFF6F7F9),
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: getProportionateScreenWidth(20)),
-                          child: (garage.ratingGarages != null &&
-                                  garage.ratingGarages!.isNotEmpty)
-                              ? Text(
-                                  "Reviews (" +
-                                      garage.ratingGarages!.length.toString() +
-                                      ")",
-                                  style: Theme.of(context).textTheme.headline6,
-                                )
-                              : Text(
-                                  "Reviews (0)",
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                        ),
-                        (garage.ratingGarages != null &&
-                                garage.ratingGarages!.isNotEmpty)
-                            ? ListView.separated(
-                                separatorBuilder:
-                                    (BuildContext context, int index) =>
-                                        const Divider(),
-                                padding: EdgeInsets.all(
-                                    getProportionateScreenWidth(15)),
-                                physics: const NeverScrollableScrollPhysics(),
-                                shrinkWrap: true,
-                                itemCount: 10,
-                                itemBuilder: (ctx, index) => ListTile(
-                                  onTap: () {},
-                                  leading: Image.network(
-                                      "https://picsum.photos/100?random=$index"),
-                                  title: Text('User Name $index'),
-                                  subtitle: Text('Cửa hàng phụ vụ rất \ntốt'),
-                                  trailing: RoundedIconBtn(
-                                    icon: Icons.thumb_up,
-                                    press: () {},
+    return Consumer<GarageProvider>(
+      builder: (context, value, child) => ListView(
+        children: [
+          SliderImages(garage: garage),
+          TopRoundedContainer(
+            color: Colors.white,
+            child: Column(
+              children: [
+                GarageDescription(
+                  garage: garage,
+                  pressOnSeeMore: () {},
+                ),
+                TopRoundedContainer(
+                  color: Color(0xFFF6F7F9),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: getProportionateScreenWidth(20)),
+                            child: (garage.ratingGarages != null &&
+                                    garage.ratingGarages!.isNotEmpty)
+                                ? Text(
+                                    "Reviews (" +
+                                        garage.ratingGarages!.length
+                                            .toString() +
+                                        ")",
+                                    style:
+                                        Theme.of(context).textTheme.headline6,
+                                  )
+                                : Text(
+                                    "Reviews (0)",
+                                    style:
+                                        Theme.of(context).textTheme.headline6,
                                   ),
-                                  isThreeLine: true,
-                                ),
-                              )
-                            : const Padding(
-                                padding: EdgeInsets.only(top: 10),
-                                child:
-                                    Text("Chưa có đánh giá nào về gara này."),
-                              )
-                      ],
-                    ),
-                    TopRoundedContainer(
-                      color: Colors.white,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          left: SizeConfig.screenWidth * 0.15,
-                          right: SizeConfig.screenWidth * 0.15,
-                          bottom: getProportionateScreenWidth(40),
-                          top: getProportionateScreenWidth(15),
-                        ),
-                        child: DefaultButton(
-                          text: "View on map",
-                          press: () {},
+                          ),
+                          (garage.ratingGarages != null &&
+                                  garage.ratingGarages!.isNotEmpty)
+                              ? ListView.separated(
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          const Divider(),
+                                  padding: EdgeInsets.all(
+                                      getProportionateScreenWidth(15)),
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  itemCount: 10,
+                                  itemBuilder: (ctx, index) => ListTile(
+                                    onTap: () {},
+                                    leading: Image.network(
+                                        "https://picsum.photos/100?random=$index"),
+                                    title: Text('User Name $index'),
+                                    subtitle: Text('Cửa hàng phụ vụ rất \ntốt'),
+                                    trailing: RoundedIconBtn(
+                                      icon: Icons.thumb_up,
+                                      press: () {},
+                                    ),
+                                    isThreeLine: true,
+                                  ),
+                                )
+                              : const Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child:
+                                      Text("Chưa có đánh giá nào về gara này."),
+                                )
+                        ],
+                      ),
+                      TopRoundedContainer(
+                        color: Colors.white,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: SizeConfig.screenWidth * 0.15,
+                            right: SizeConfig.screenWidth * 0.15,
+                            bottom: getProportionateScreenWidth(40),
+                            top: getProportionateScreenWidth(15),
+                          ),
+                          child: DefaultButton(
+                            text: "View on map",
+                            press: () {},
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
