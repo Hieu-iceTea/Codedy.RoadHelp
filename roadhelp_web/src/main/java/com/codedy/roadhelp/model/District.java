325f7fec4349f669b1bfe271e9a1ae0fe7660872
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-24 16:41:36 +0700

    UPDATE: Format code (model)

--- roadhelp_web/src/main/java/com/codedy/roadhelp/model/District.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/model/District.java
@@ -99,6 +99,15 @@ public class District extends BaseModel implements Serializable {
         return hashMap;
     }
 
+    /**
+     * Hàm này trả về định dạng hiển thị dữ liệu API cho entity này.<br/><br/>
+     * <p>
+     * Viết bởi: Hiếu iceTea<br/>
+     * Ngày: 24-10-2021<br/>
+     * Thời gian: 10:15<br/>
+     *
+     * @return
+     */
     public LinkedHashMap<String, Object> toApiResponse() {
         LinkedHashMap<String, Object> hashMap = this.toHashMap();
 
