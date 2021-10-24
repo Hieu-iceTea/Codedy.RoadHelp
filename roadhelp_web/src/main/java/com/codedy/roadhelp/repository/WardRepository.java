815228b41411161f300639d2c3a00a60dab742e2
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-24 18:49:36 +0700

    NEW: viết truy vấn findBy ở trong Repository

--- roadhelp_web/src/main/java/com/codedy/roadhelp/repository/WardRepository.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/repository/WardRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface WardRepository extends BaseRepository<Ward, Integer>{
 
     //Danh sách Xã trong Huyện
-    List<Ward> findAllByDistrictId(int district_id); // 1. Xã trong huyện
-    List<Ward> findAllByDistrictIdAndProvinceId(int district_id, int province_id); // 2. Xã trong huyện & tỉnh
+    List<Ward> findAllByDistrictId(int district_id); // 1. Nếu chỉ truyền ID_huyện
+    List<Ward> findAllByDistrictIdAndProvinceId(int district_id, int province_id); // 2. Nếu truyền cả ID_huyện & ID_tỉnh
 
 }
