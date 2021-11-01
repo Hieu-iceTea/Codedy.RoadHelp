8212a5a6d1ed49c429c3aac76986bfcb43e61648
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-11-01 23:01:53 +0700

    NEW: PopupMenuButton -> IssueListScreenState { receiveNew, historySent, historyReceived }

--- roadhelp_app/lib/screens/emergency_rescue/receive/receive_rescue/components/body.dart
+++ roadhelp_app/lib/screens/emergency_rescue/receive/receive_rescue/components/body.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:roadhelp/helper/util.dart';
-import 'package:roadhelp/models/garage.dart';
 import 'package:roadhelp/models/issue.dart';
-import 'package:roadhelp/providers/garage_provider.dart';
 import 'package:roadhelp/providers/issue_provider.dart';
 
 import '/config/size_config.dart';
+import '../receive_rescue_screen.dart';
 import 'issue_list_item.dart';
 
 class Body extends StatefulWidget {
+  IssueListScreenState issueListScreenState;
+
+  Body({
+    required this.issueListScreenState,
+    Key? key,
+  }) : super(key: key);
+
   @override
   _BodyState createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
   Future<List<Issue>> _fetchAllData(BuildContext context) async {
-    return await Provider.of<IssueProvider>(context, listen: false)
-        .fetchAllDataByStatusSent();
+    if (widget.issueListScreenState == IssueListScreenState.receiveNew) {
+      return await Provider.of<IssueProvider>(context, listen: false)
+          .fetchAllDataByStatusSent();
+    }
+
+    if (widget.issueListScreenState == IssueListScreenState.historySent) {
+      return await Provider.of<IssueProvider>(context, listen: false)
+          .fetchAllDataByUserMember();
+    }
+
+    if (widget.issueListScreenState == IssueListScreenState.historyReceived) {
+      return await Provider.of<IssueProvider>(context, listen: false)
+          .fetchAllDataByUserPartner();
+    }
+
+    return [];
   }
 
   @override
