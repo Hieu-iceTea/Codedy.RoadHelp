38df550dd64a567cfca96e3251db47247eefc28b
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-11-07 21:26:24 +0700

    UPDATE: thông báo khi ấn "trở thành đối tác" khi đã gửi yêu cầu rồi

--- roadhelp_app/lib/screens/my_account/become_to_partner/components/body.dart
+++ roadhelp_app/lib/screens/my_account/become_to_partner/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:roadhelp/providers/auth_provider.dart';
@@ -10,30 +11,42 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<AuthProvider>(
-      builder: (context, authProvider, child) => SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(height: SizeConfig.screenHeight * 0.03),
-                  Text("Đăng kí đối tác", style: headingStyle),
-                  Text(
-                    "Vui lòng xác nhận lại thông tin\nđể đăng kí làm đối tác.",
-                    textAlign: TextAlign.center,
+      builder: (context, authProvider, child) =>
+          !authProvider.authData.currentUser!.requestBecomePartner!
+              ? SafeArea(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20)),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            SizedBox(height: SizeConfig.screenHeight * 0.03),
+                            Text("Đăng kí đối tác", style: headingStyle),
+                            Text(
+                              "Vui lòng xác nhận lại thông tin\nđể đăng kí làm đối tác.",
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: SizeConfig.screenHeight * 0.06),
+                            BeComeToPartnerForm(
+                                user: authProvider.authData.currentUser),
+                            SizedBox(height: getProportionateScreenHeight(30)),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                  SizedBox(height: SizeConfig.screenHeight * 0.06),
-                  BeComeToPartnerForm(user: authProvider.authData.currentUser),
-                  SizedBox(height: getProportionateScreenHeight(30)),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+                )
+              : const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: Center(
+                    child: Text(
+                      "Bạn đã gửi yêu cầu trở thành đối tác. Chúng tôi đang xem xét và phê duyệt nó.\nCảm ơn về sự kiên nhẫn của bạn!",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
     );
   }
 }
