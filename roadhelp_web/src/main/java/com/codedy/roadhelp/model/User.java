6cac7f9799a08aff8ae1beaa9ea6f9d704eace22
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-11-08 10:54:34 +0700

    UPDATE fix validate email

--- roadhelp_web/src/main/java/com/codedy/roadhelp/model/User.java
+++ roadhelp_web/src/main/java/com/codedy/roadhelp/model/User.java
@@ -4,6 +4,7 @@ import com.codedy.roadhelp.model.enums.UserGender;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -20,7 +21,8 @@ public class User extends BaseModel implements Serializable {
     @Size(min = 6, max = 64)
     private String username;
 
-    @Email
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @NotEmpty(message = "Email cannot be empty")
     @Size(max = 128)
     private String email;
 
