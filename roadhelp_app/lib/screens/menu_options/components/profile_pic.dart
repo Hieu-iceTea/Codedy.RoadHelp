7f24510db8bffa27b475aea854abc8bc13cea693
Vũ Quang Huy <vuquanghuyxl1234@gmail.com>
2021-11-12 00:55:47 +0700

    UPDATE: xóa nút edit ảnh ở menu option

--- roadhelp_app/lib/screens/menu_options/components/profile_pic.dart
+++ roadhelp_app/lib/screens/menu_options/components/profile_pic.dart
@@ -26,26 +26,6 @@ class ProfilePic extends StatelessWidget {
                   backgroundImage:
                       AssetImage("assets/images/Profile Image.png"),
                 ),
-          Positioned(
-            right: -16,
-            bottom: 0,
-            child: SizedBox(
-              height: 46,
-              width: 46,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  primary: Colors.white,
-                  backgroundColor: Color(0xFFF5F6F9),
-                ),
-                onPressed: () {},
-                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
-              ),
-            ),
-          )
         ],
       ),
     );
