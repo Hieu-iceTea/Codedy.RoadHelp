275e8e787260907aad76eaf587dd71361ee67712
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-11-15 18:09:24 +0700

    NEW: NotificationScreen

--- roadhelp_app/lib/config/routes.dart
+++ roadhelp_app/lib/config/routes.dart
@@ -36,6 +36,7 @@ import '/screens/emergency_rescue/user_info/user_info_screen.dart';
 import '/screens/emergency_rescue/issue_details/issue_details_screen.dart';
 import '/screens/admin/confirm_become_partner/user_request_become_partner/user_request_become_partner_screen.dart';
 import '/screens/admin/confirm_become_partner/user_details/user_details_screen.dart';
+import '/screens/notification/notification_screen.dart';
 
 // We use name route
 // All our routes will be available here
@@ -77,4 +78,5 @@ final Map<String, WidgetBuilder> routes = {
   IssueDetailsScreen.routeName: (context) => const IssueDetailsScreen(),
   UserRequestBecomePartnerScreen.routeName: (context) => const UserRequestBecomePartnerScreen(),
   UserDetailsScreen.routeName: (context) => const UserDetailsScreen(),
+  NotificationScreen.routeName: (context) => const NotificationScreen(),
 };
